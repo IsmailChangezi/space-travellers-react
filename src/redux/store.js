586d1dc78dc8d62072b873bc69738256f0051a76
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-12 02:53:01 +0500

    Update the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rocketSlice from "./rocket/rocketSlice";
+import missionSlice from "./missions/missionSlice";
 
 export const store = configureStore({
-  reducer: rocketSlice,
+  reducer: {
+    rockets: rocketSlice,
+    missions: missionSlice,
+  },
 });
