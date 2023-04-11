28f81ec60cbaaec3d81993c202c33683880bfc05
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-12 02:51:55 +0500

    Call for dispatch

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMissionData } from "../redux/missions/missionSlice";
 
 export default function Missions() {
+  const { data, loading, error } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissionData());
+  }, []);
+  console.log(data);
   return <div>Missions</div>;
 }
