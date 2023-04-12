156168936365db8713c3408ce83448f9f359bdf2
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-12 17:18:05 +0500

    Add links to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Missions from "./Missions";
-import Profile from "./Profile";
-import Rockets from "./Rockets";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
-    <header>
-      <Link to="/">
-        <h1>BookStore CMS</h1>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Rockets</Link>
-          </li>
-          <li>
-            <Link to="/Profile">Profiles</Link>
-          </li>
-          <li>
-            <Link to="/Missions">Missions</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <Link to="/">
+          <h3>Space Traveller&#39;s Hub</h3>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Rockets</Link>
+            </li>
+            <li>
+              <Link to="/Profile">Profiles</Link>
+            </li>
+            <li>
+              <Link to="/Missions">Missions</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <hr />
+    </>
   );
 }
