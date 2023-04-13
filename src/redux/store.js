f018457dfef55a646cbe64c199f2edae948e4171
Omar Farouq <ofarouq310@gmail.com>
2023-04-13 11:08:55 +0200

    Add missisons reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketSlice from './rocket/rocketSlice';
+import missions from './missions/missions';
 
 const store = configureStore({
   reducer: {
     rockets: rocketSlice,
+    missionsReducer: missions,
   },
 });
 
