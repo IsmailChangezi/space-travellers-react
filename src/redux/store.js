3d51458bd988732f233f014f51108841cd120466
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-13 12:51:04 +0500

    Fix Linter errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rocketSlice from './rocket/rocketSlice';
-import missionSlice from './missions/missionSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import rocketSlice from "./rocket/rocketSlice";
 
 const store = configureStore({
   reducer: {
     rockets: rocketSlice,
-    missions: missionSlice,
   },
 });
 
