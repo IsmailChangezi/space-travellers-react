6b5dd6f24e1cb4a7eb4bdc76ea06fb20bfdb0cfa
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-13 12:49:43 +0500

    Fix Linter errors

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissionData } from '../redux/missions/missionSlice';
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchMissionData } from "../redux/missions/missionSlice";
 
-export default function Missions() {
-  const { data } = useSelector((state) => state.missions);
-  const dispatch = useDispatch();
+// export default function Missions() {
+//   const { data } = useSelector((state) => state.missions);
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchMissionData());
-  }, []);
-  console.log(data);
-  return <div>Missions</div>;
-}
+//   useEffect(() => {
+//     dispatch(fetchMissionData());
+//   }, []);
+
+//   return <div>Missions</div>;
+// }
