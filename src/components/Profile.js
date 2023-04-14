f68519fc44c46e9a658c7ef34eb0ee7ad9a9f177
Ismail Ali <ismailchangezi801@gmail.com>
2023-04-14 18:57:20 +0500

    Show the reserved Rockets on the profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default function Profile() {
-  return <div>Profile</div>;
-}
+const Profile = () => {
+  // const missions = useSelector((state) => state.missions.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  // const missionMembers = missions.filter((mission) => mission.member);
+
+  return (
+    <div>
+      <ul className="w-50">
+        <h1>My Rockets</h1>
+        {reservedRockets.map((rocket) => (
+          <li key={rocket.id} className="p-1 text-2xl border">
+            {rocket.rocket_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Profile;
